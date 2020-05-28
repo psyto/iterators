634d285fb46c6d1d18f8d380462dd12399593ea1
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2020-05-28 17:24:16 +0900

    implemented till 25 min

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,24 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+pub fn flatten<I>(iter: I) -> Flatten<I> {
+    Flatten::new(iter)
+}
+
+pub struct Flatten<O> {
+    outer: O,
+}
+
+impl<O> Flatten<O> {
+    fn new(iter: O) -> Self {
+        Flatten { outer: iter }
+    }
+}
+
+impl<O> Iterator for Flatten<O>
+where
+    O: Iterator,
+    O::Item: IntoIterator,
+{
+    type Item = <O::Item as IntoIterator>::Item;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.outer.next().and_then(|inner| inner.into_iter().next())
     }
 }
